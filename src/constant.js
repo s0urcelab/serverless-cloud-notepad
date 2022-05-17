8614ed80634a33ecec19c2e18b74c5e4c40a5744
s0urcelab <me@src.moe>
2022-05-17 21:01:04 +0800

    feat: major update to 2.0.0

---
+++ src/constant.js
@@ -0,0 +1,8 @@
+// static CDN
+export const CDN_PREFIX = 'cdn.jsdelivr.net/gh/s0urcelab/serverless-cloud-notepad@2.0.0/static'
+
+// server side salt
+export const SALT = 'gshkol3k8bjhgadf'
+
+// server side secret
+export const SECRET = 'bnufy8hsd67r2za6'
--- src/index.js
+++ src/index.js
@@ -1,121 +1,192 @@
-// use my repo jsdelivr or you can set cdn yourself
-const CDN_PREFIX = 'cdn.jsdelivr.net/gh/s0urcelab/serverless-cloud-notepad@site/static'
+import dayjs from 'dayjs'
+import { Router } from 'itty-router'
+import Cookies from 'cookie'
+import jwt from '@tsndr/cloudflare-worker-jwt'
+import { queryNote, MD5, checkAuth, genRandomStr, returnPage, returnJSON, saltPw } from './helper'
+import { SECRET } from './constant'
+
+// init
+const router = Router()
+
+router.get('/', ({ url }) => {
+    const newHash = genRandomStr(3)
+    // redirect to new page
+    return Response.redirect(`${url}${newHash}`, 302)
+})
+
+router.get('/share/:md5', async ({ params }) => {
+    const { md5 } = params
+    const path = await SHARE.get(md5)
+
+    if (!!path) {
+        const { value, metadata } = await queryNote(path)
+
+        return returnPage('Share', {
+            title: decodeURIComponent(path),
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    return returnPage('Page404', { title: '404' })
+})
+
+router.get('/:path', async ({ params, headers }) => {
+    const { path } = params
+    const title = decodeURIComponent(path)
+
+    const cookie = Cookies.parse(headers.get('Cookie') || '')
+
+    const { value, metadata } = await queryNote(path)
+
+    if (!metadata.pw) {
+        return returnPage('Edit', {
+            title,
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    const valid = await checkAuth(cookie, path)
+    if (valid) {
+        return returnPage('Edit', {
+            title,
+            content: value,
+            ext: metadata,
+        })
+    }
+
+    return returnPage('NeedPasswd', { title })
+})
+
+router.post('/:path/auth', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const { passwd } = await request.json()
+
+        const { metadata } = await queryNote(path)
+
+        if (metadata.pw) {
+            const storePw = await saltPw(passwd)
+
+            if (metadata.pw === storePw) {
+                const token = await jwt.sign({ path }, SECRET)
+                return returnJSON(0, {
+                    refresh: true,
+                }, {
+                    'Set-Cookie': Cookies.serialize('auth', token, {
+                        path: `/${path}`,
+                        expires: dayjs().add(7, 'day').toDate(),
+                        httpOnly: true,
+                    })
+                })
+            }
+        }
+    }
+
+    return returnJSON(10002, '密码验证失败！')
+})
+
+router.post('/:path/pw', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+        const { passwd } = await request.json()
+
+        const { value, metadata } = await queryNote(path)
+        const valid = await checkAuth(cookie, path)
+        
+        if (!metadata.pw || valid) {
+            const pw = passwd ? await saltPw(passwd) : undefined
+            try {
+                await NOTES.put(path, value, {
+                    metadata: {
+                        ...metadata,
+                        pw,
+                    },
+                })
+    
+                return returnJSON(0, null, {
+                    'Set-Cookie': Cookies.serialize('auth', '', {
+                        path: `/${path}`,
+                        expires: dayjs().subtract(100, 'day').toDate(),
+                        httpOnly: true,
+                    })
+                })
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        return returnJSON(10003, '密码设置失败！')
+    }
+})
+
+router.post('/:path/setting', async request => {
+    const { path } = request.params
+    if (request.headers.get('Content-Type') === 'application/json') {
+        const cookie = Cookies.parse(request.headers.get('Cookie') || '')
+        const { mode, share } = await request.json()
+
+        const { value, metadata } = await queryNote(path)
+        const valid = await checkAuth(cookie, path)
+        
+        if (!metadata.pw || valid) {
+            try {
+                await NOTES.put(path, value, {
+                    metadata: {
+                        ...metadata,
+                        ...mode !== undefined && { mode },
+                        ...share !== undefined && { share },
+                    },
+                })
+
+                const md5 = await MD5(path)
+                if (share) {
+                    await SHARE.put(md5, path)
+                    return returnJSON(0, md5)
+                }
+                if (share === false) {
+                    await SHARE.delete(md5)
+                }
+
+
+                return returnJSON(0)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        return returnJSON(10004, '更新设置失败！')
+    }
+})
+
+router.post('/:path', async request => {
+    const { path } = request.params
+
+    const formData = await request.formData();
+    const content = formData.get('t')
+
+    const { metadata } = await queryNote(path)
+
+    try {
+        await NOTES.put(path, content, {
+            metadata: {
+                ...metadata,
+                updateAt: dayjs().unix(),
+            },
+        })
+
+        return returnJSON(0)
+    } catch (error) {
+        console.error(error)
+    }
+
+    return returnJSON(10001, 'KV insert fail!')
+})
+
+router.all('*', () => returnPage('Page404', { title: '404' }))
 
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request));
-});
-
-const genHash = n => {
-    // remove char that confuse user
-    const charset = '2345679abcdefghjkmnpqrstwxyz'
-    return Array(n)
-        .join()
-        .split(',')
-        .map(() => charset.charAt(Math.floor(Math.random() * charset.length)))
-        .join('')
-}
-
-const html = (path, content) => `
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>${decodeURI(path)} — Cloud Notepad</title>
-    <link href="//${CDN_PREFIX}/favicon.ico" rel="shortcut icon" type="image/ico" />
-    <link href="//${CDN_PREFIX}/css/normalize.min.css" rel="stylesheet" media="screen" />
-    <link href="//${CDN_PREFIX}/css/app.min.css" rel="stylesheet" media="screen" />
-</head>
-<body>
-    <div class="stack">
-        <div class="layer_1">
-            <div class="layer_2">
-                <div class="layer_3">
-                    <textarea name="contents" id="contents" class="contents" spellcheck="true" placeholder="看来你是第一个到这儿的人，写点什么吧...">${content}</textarea>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div id="loading"></div>
-    <div class="footer-github-link">
-        <a title="github" target="_blank" href="https://github.com/s0urcelab/serverless-cloud-notepad" rel="noreferrer"><span role="img" aria-label="github" class="anticon anticon-github"><svg viewBox="64 64 896 896" focusable="false" data-icon="github" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M511.6 76.3C264.3 76.2 64 276.4 64 523.5 64 718.9 189.3 885 363.8 946c23.5 5.9 19.9-10.8 19.9-22.2v-77.5c-135.7 15.9-141.2-73.9-150.3-88.9C215 726 171.5 718 184.5 703c30.9-15.9 62.4 4 98.9 57.9 26.4 39.1 77.9 32.5 104 26 5.7-23.5 17.9-44.5 34.7-60.8-140.6-25.2-199.2-111-199.2-213 0-49.5 16.3-95 48.3-131.7-20.4-60.5 1.9-112.3 4.9-120 58.1-5.2 118.5 41.6 123.2 45.3 33-8.9 70.7-13.6 112.9-13.6 42.4 0 80.2 4.9 113.5 13.9 11.3-8.6 67.3-48.8 121.3-43.9 2.9 7.7 24.7 58.3 5.5 118 32.4 36.8 48.9 82.7 48.9 132.3 0 102.2-59 188.1-200 212.9a127.5 127.5 0 0138.1 91v112.5c.8 9 0 17.9 15 17.9 177.1-59.7 304.6-227 304.6-424.1 0-247.2-200.4-447.3-447.5-447.3z"></path></svg></span></a>
-    </div>
-    <script src="//${CDN_PREFIX}/js/app.min.js"></script>
-</body>
-</html>
-`
-
-async function returnPage(path, content) {
-    return new Response(html(path, content), {
-        headers: {
-            'content-type': 'text/html;charset=UTF-8',
-        },
-    });
-}
-
-async function returnJSON(json) {
-    return new Response(JSON.stringify(json), {
-        headers: {
-            'content-type': 'application/json;charset=UTF-8',
-        },
-    })
-}
-
-async function handleRequest(request) {
-    const url = new URL(request.url);
-
-    // GET read note
-    if (request.method === 'GET') {
-        // random new hash
-        if (url.pathname === '/') {
-            const newHash = genHash(3)
-
-            // redirect to new page
-            return Response.redirect(`${url.href}${newHash}`, 302);
-        }
-
-        const slashIdx = url.pathname.slice(1).indexOf('/')
-        const nextPath = !!~slashIdx ? url.pathname.slice(0, slashIdx + 1) : url.pathname
-
-        // redirect to fixed path
-        if (url.pathname !== nextPath) {
-            return Response.redirect(`${url.href}${nextPath}`, 302);
-        }
-
-        // query content
-        const queryKey = url.pathname.slice(1)
-        const content = await NOTES.get(queryKey) || ''
-
-        return returnPage(queryKey, content)
-    }
-
-    // POST modify note
-    if (request.method === 'POST') {
-        let saveFlag = true
-
-        const formData = await request.formData();
-        const saveText = formData.get('t')
-        const savePath = url.pathname.slice(1)
-
-        try {
-            await NOTES.put(savePath, saveText)
-        } catch (error) {
-            console.log(error)
-            saveFlag = false
-        }
-
-        const success = {
-            errCode: 0,
-            message: 'ok',
-            padname: savePath,
-        }
-
-        const fail = {
-            errCode: 1,
-            message: 'kv database put error!',
-        }
-
-        return returnJSON(saveFlag ? success : fail)
-    }
-}
+    event.respondWith(router.handle(event.request))
+})
